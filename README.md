06144e1b551d0e2623adffbc4ffa6f8d5b65dc49
ampersarnie <hello@ampersandwich.co.uk>
2014-06-13 13:16:31 +0100

    Made improvements to gint and ghint functions, added checks for GH access tokens and updated README

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ To add these to your zsh setup simply add the following to your `~/.zshrc` file 
 For example: `source ~/.zsh/git.zsh`
 
 You will end up with an `~/.zshrc` file that looks a bit like this:
-
 ```
 export ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="agnoster"
@@ -18,3 +17,35 @@ source ~/.zshvars
 source ~/.zsh/git.zsh
 …
 ```
+###### Github
+A couple of these scripts require a Github API Access Token using the `GITHUB_ACCESS_TOKEN` variable. You can [https://github.com/settings/applications#personal-access-tokens](generate a token here). Define your token either within `.zshrc` or create a new file specifically for sensitive variables. I use a `.zshvars` file in the home directory and include it as above.
+
+# How to use the Commands
+### git.zsh
+#### gint [\<name> \<url>\] (Git Init)
+The `gint` command allows you to create a git repository within the current working directory, adds a `README.md` and `.gitignore`. Optionally you can also add a remote repository.
+###### Create a repository:
+`gint`
+###### Create a repository and add remote
+`gint origin https://example.com/username/repo`
+
+#### ghint [\<api key name>:\<your value> … \] (Github Init)
+The `ghint` command is very much the same as the `gint` comment except it will also create a Github repository using the passed arguments. `ghint` requires a minimum argument of `name:<value>`.
+###### Create a repository with the name “My Amazing Repo”:
+`ghint name:”My Amazing Repo”`
+###### Create a private repository
+`ghint name:”Private Parts” private:true`
+###### Create a repository without a wiki and add a description
+`ghint name:”Awesome Source” has_wiki:false description:”This is where I add all the awesome.”`
+
+A whole list of accepted API Key Names can be found [https://developer.github.com/v3/repos/#create](here).
+
+#### catt [\<commit message>\] (Commit All The Things)
+`catt` allows you to commit every file in the current working repository. Once committed the log for that commit will be outputted.
+###### Commit Everything:
+`catt “ZOMG I COMMITTED EVERYTHING!”`
+
+#### satt [\<stash name>\] (Stash All The Things)
+`satt` will allow you to easily stash all the files in the current working repository with a given name. The output will be list of all files added to the stash.
+###### Stash Everything:
+`catt “stash-ah-ahhh”`
--- git.zsh
+++ git.zsh
@@ -24,16 +24,21 @@ gint() {
 # Github Init
 # - Initializes git in the current directory, creates a README and .gitignore
 #   then adds a repository to github.com and pushes.
-# Usage: ghint <repo name>
-# Example: $ ghint "My Repository"
+# - Requires API Token to be set.
+# Usage: ghint <api key>:<value>
+# Example: $ ghint name:"My Repository" description:"this is my repository, it's a very fine one."
 ghint() {
-    if [[ ! -z $1 ]];
+    # gh-has-access-token;
+
+    if [[ ! -z $@ ]];
         then
             touch README.md
             echo "# "$1 >> README.md
             touch .gitignore
             echo ".gitignore" >> .gitignore
-            repo=$(gh-create-repo $1 2>&1)
+
+            # Create the repo on github.
+            repo=$(gh-create-repo $@ 2>&1)
 
             git init
             git add .
@@ -50,20 +55,57 @@ ghint() {
 # - Makes an API call to Github, creates the given repository name
 #   and outputs the SSH URL.
 # - Requires API Token to be set.
-# Usage: gh-create-repo <repo name>
-# Example: $ gh-create-repo "My Repository"
+# Usage: gh-create-repo <api key>:<value>
+# Example: $ gh-create-repo name:"My Repository" description:"this is my repository, it's a very fine one."
 gh-create-repo() {
-    if [[ ! -z $1 ]];
+
+    hasname=false
+
+    # Create the request JSON by looping through all passed
+    # arguments.
+    COUNT=0;
+    request="{"
+    for ARG in "$@"
+    do
+        # Check for "name" in the current argument.
+        name=$(echo $ARG | sed -E "s/^(.*):(.*)$/\1/")
+        if [ $name = "name" ];
+            then
+                hasname=true;
+        fi
+
+        addition=$(echo $ARG | sed -E "s/^(.*):(.*)$/\"\1\":\"\2\"/")
+
+        if [[ $COUNT > 0 ]];
+            then
+                request=$request","$addition
+        else
+            request=$request$addition
+        fi
+
+        COUNT=$COUNT+1
+    done
+    request=$request"}"
+
+    # If $hasname is true then check for access token,
+    # otherwise echo message and ctrl-c
+    if $hasname
         then
-            # Create Github Repo
-            output=$(curl -s -u $GITHUB_ACCESS_TOKEN":x-oauth-basic" https://api.github.com/user/repos -d "{\"name\": \""$1"\"}" 2>&1)
-            sshurl=$(echo $output | egrep -o "\"ssh_url\": \"(.*)\"," | sed -E "s/^\"ssh_url\": \"(.*)\",/\1/")
-
-            echo $sshurl
-
+            gh-has-access-token;
     else
-        echo "$fg[red]Oops! You haven't defined a repo."
-        echo "Usage: gh-create-repo <repo name>"
+        echo "$fg[red]An argument of 'name:\"Repository Name\"' is required to be able to create a repository on Github."
+        kill -INT $$
+    fi
+
+    # Create Github Repo
+    output=$(curl -s -u $GITHUB_ACCESS_TOKEN":x-oauth-basic" https://api.github.com/user/repos -d $request 2>&1)
+    sshurl=$(echo $output | egrep -o "\"ssh_url\": \"(.*)\"," | sed -E "s/^\"ssh_url\": \"(.*)\",/\1/")
+
+    if [ "$sshurl" ];
+        then
+            echo $sshurl
+    else
+        echo "$fg[red]There was a problem creating the repo on Github."
     fi
 }
 
@@ -98,4 +140,17 @@ satt() {
 # Usage: gsl
 gsl() {
     output=$(git stash show -p | grep 'diff'); echo $output | egrep -o " b\/(.*)"
+}
+
+# Github: Has Access Token
+gh-has-access-token() {
+    if [[ -z $GITHUB_ACCESS_TOKEN ]];
+        then
+            echo "$fg[red]Please set the GITHUB_ACCESS_TOKEN variable."
+    fi
+
+    if [[ -z $GITHUB_ACCESS_TOKEN ]];
+        then
+            kill -INT $$
+    fi
 }
